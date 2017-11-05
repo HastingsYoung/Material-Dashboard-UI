1d38542dadf95f5d8c6816d4037e1c8328922bb2
Guangling.Hastings <hastings_gl@outlook.com>
2017-11-06 01:03:02 +1100

    Add Pagination

--- src/containers/Log/Log.jsx
+++ src/containers/Log/Log.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
+import FlatButton from 'material-ui/FlatButton';
 import './log.css';
 
 const reg = {
@@ -12,11 +12,15 @@ const reg = {
 
 export default class Log extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            text : `Loading...`
-        }
+            text: `Loading...`,
+            records: [],
+            page: 0,
+            max: 0,
+            maxLines: 40
+        };
     }
 
     componentDidMount() {
@@ -25,21 +29,42 @@ export default class Log extends Component {
         fetch("http://localhost:8080/dashboard/logs", {
             method: "GET",
             mode: 'cors'
-        }).then((res)=> {
+        }).then((res) => {
             return res.text();
-        }).then((text)=> {
-            self.setState({text});
+        }).then((text) => {
+            let records = text.split(/\n/);
+            let max = (Math.floor(records.length / this.state.maxLines));
+            self.setState({text, records, max});
         });
+    }
+
+    toPage(i) {
+        if (i >= 0 && i <= this.state.records.length / this.state.maxLines)
+            this.setState({page: i});
     }
 
     render() {
 
-        const records = this.state.text.split(/\n/);
+        const from = this.state.page * this.state.maxLines;
+        const to = (this.state.page + 1) * this.state.maxLines;
+        const records = this.state.records.slice(from, to);
 
         return (<div className="log">
-            {records.map((d, i)=> {
-                return <LogRecord key={i} text={d}/>;
-            })}
+            <Pagination curr={this.state.page} max={this.state.max} first={this.toPage.bind(this, 0)}
+                        last={this.toPage.bind(this, this.state.max)}
+                        next={this.toPage.bind(this, (this.state.page + 1))}
+                        previous={this.toPage.bind(this, (this.state.page - 1))}/>
+
+            <div className="log-container">
+                {records.map((d, i) => {
+                    return <LogRecord key={i} text={d}/>;
+                })}
+            </div>
+            
+            <Pagination curr={this.state.page} max={this.state.max} first={this.toPage.bind(this, 0)}
+                        last={this.toPage.bind(this, this.state.max)}
+                        next={this.toPage.bind(this, (this.state.page + 1))}
+                        previous={this.toPage.bind(this, (this.state.page - 1))}/>
         </div>);
     }
 }
@@ -60,8 +85,24 @@ class LogRecord extends Component {
             type += " normal"
         }
 
-        return (<div className={"log-record"+type}>
+        return (<div className={"log-record" + type}>
             {this.props.text}
+        </div>);
+    }
+}
+
+class Pagination extends Component {
+    render() {
+        return (<div className="log-pagination">
+            <div className="log-pagination-legend">
+                Page {this.props.curr + 1} of {this.props.max + 1}
+            </div>
+            <div className="log-pagination-btns">
+                <FlatButton label="First" style={{borderRadius: 20}} onClick={this.props.first}/>
+                <FlatButton label="Previous" style={{borderRadius: 20}} onClick={this.props.previous}/>
+                <FlatButton label="Next" style={{borderRadius: 20}} onClick={this.props.next}/>
+                <FlatButton label="Last" style={{borderRadius: 20}} onClick={this.props.last}/>
+            </div>
         </div>);
     }
 }
